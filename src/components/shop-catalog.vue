b65189d4048622a7d037663a02a73ddbf38ba3fd
Gutsss <kacill1@mail.ru>
2023-01-26 11:58:50 +0300

    add response at products

--- src/components/shop-catalog.vue
+++ src/components/shop-catalog.vue
@@ -38,6 +38,11 @@ export default {
   },
   mounted() {
     this.GET_PRODUCTS_FROM_API()
+    .then((response) => {
+        if (response.data) {
+            console.log("Data arrived")
+        }
+    })
   }
 };
 </script>
